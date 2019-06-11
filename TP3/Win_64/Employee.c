817d3989c0d39405fa5f46733349fe4aced25180
tomaszorzano <40808662+tomaszorzano@users.noreply.github.com>
2019-06-10 22:08:20 -0300

    Add files via upload

---
+++ TP3/Win_64/Employee.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "LinkedList.h"
+#include "Controller.h"
+#include "Employee.h"
+#define OK 1
+#define ERROR 0
+
+
+Employee* employee_new()
+{
+    Employee* emp;
+    emp = (Employee*)malloc(sizeof(Employee));
+    emp->id = 0;
+    strcpy(emp->nombre, "");
+    emp->horasTrabajadas = 0;
+    emp->sueldo = 0;
+
+    return emp;
+};
+
+Employee* employee_newParametros(char* idStr,char* nombreStr,char* horasTrabajadasStr)
+{
+    Employee* emp;
+    emp = (Employee*)malloc(sizeof(Employee));
+    emp->id = *idStr;
+    strcpy(emp->nombre,nombreStr);
+    emp->horasTrabajadas = *horasTrabajadasStr;
+    emp->sueldo = 0;
+
+    return emp;
+};
+
+void employee_delete()
+{
+
+};
+
+int employee_setId(Employee* this,int id)
+{
+    int ret;
+    ret = ERROR;
+
+    if( id > 0 && this != NULL)
+    {
+        this->id = id;
+        ret=OK;
+    }
+
+    return ret;
+};
+
+int employee_getId(Employee* this,int* id)
+{
+    int ret;
+    ret = ERROR;
+
+    if( this != NULL )
+    {
+        *id= this->id;
+        ret = OK;
+
+    }
+
+    return ret;
+};
+
+int employee_setNombre(Employee* this,char* nombre)
+{
+    int ret;
+    ret = ERROR;
+
+    if(this != NULL)
+    {
+        strcpy(this->nombre, nombre);
+        ret=OK;
+    }
+
+    return ret;
+};
+int employee_getNombre(Employee* this,char* nombre)
+{
+  int ret;
+    ret = ERROR;
+
+    if(this != NULL)
+    {
+        strcpy(nombre,this->nombre);
+        ret=OK;
+    }
+
+    return ret;
+};
+
+int employee_setHorasTrabajadas(Employee* this,int horasTrabajadas)
+{
+     int ret;
+    ret = ERROR;
+
+    if(this != NULL)
+    {
+        this->horasTrabajadas= horasTrabajadas;
+        ret=OK;
+    }
+
+    return ret;
+};
+int employee_getHorasTrabajadas(Employee* this,int* horasTrabajadas)
+{
+   int ret;
+    ret = ERROR;
+
+    if(this != NULL)
+    {
+         *horasTrabajadas = this->horasTrabajadas;
+        ret=OK;
+    }
+
+    return ret;
+};
+int employee_setSueldo(Employee* this,int sueldo)
+{
+      int ret;
+    ret = ERROR;
+
+    if(this != NULL)
+    {
+        this->sueldo= sueldo;
+        ret=OK;
+    }
+
+    return ret;
+};
+
+int employee_getSueldo(Employee* this,int* sueldo)
+{
+    int ret;
+    ret = ERROR;
+
+    if(this != NULL)
+    {
+        *sueldo = this->sueldo;
+        ret=OK;
+    }
+
+    return ret;
+}
+
+
---
+++ TP3/Win_64/parser.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "LinkedList.h"
+#include "Employee.h"
+#define OK 1
+#define ERROR 0
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_EmployeeFromText(FILE* pFile, LinkedList* pArrayListEmployee)
+{
+
+    int ret;
+    ret = ERROR;
+    int cant = 0;
+    char idStr[6];
+    char nombreStr[20];
+    char horasTrabajadasStr[6];
+    char sueldoStr[6];
+    return 1;
+
+
+    if(pFile != NULL)
+    {
+        while(!feof(pFile))
+        {
+            cant = fscanf(pFile,"%[^,],%[^,],%[^,],%[^,\n]\n",idStr,nombreStr,horasTrabajadasStr,sueldoStr);
+            if(cant !=4)
+            {
+                printf("ERROR al cargar el archivon \n\n");
+                system("pause");
+
+            }
+            else
+            {
+                Employee* emp = employee_new();
+                emp->id = atoi(id);
+                strcpy(emp->nombre,nombreStr);
+                emp->horasTrabajadas = atoi(horasTrabajadasStr);
+                emp->sueldo = atoi(sueldoStr);
+                ll_add(pArrayListEmployee, emp);
+            }
+        }
+
+        fclose(pFile);
+        return ok;
+    }
+    else
+    {
+        printf("Archivo sin datos");
+        system("pause");
+    }
+    return ret;
+}
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_EmployeeFromBinary(FILE* pFile, LinkedList* pArrayListEmployee)
+{
+    int ret;
+    ret = ERROR;
+
+    if( pFile != NULL)
+    {
+        while (feof(pFile))
+        {
+            ret= fread(emp,sizeof(Employee),1,pFile);
+            if(ret != 1)
+            {
+                printf("Error al cargar los datos");
+
+            }
+            else
+            {
+                ll_add(pArrayListEmployee,emp);
+            }
+
+        }
+        fclose(pFile);
+        ret=OK;
+        else
+        {
+            printf("Archivo sin datos");
+            system("pause");
+        }
+
+
+        return ret;
+    }
